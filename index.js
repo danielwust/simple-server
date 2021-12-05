67fae13afa098df4b94bbf691d6044ad1fd665fc
Daniel Wust <danielwust99@gmail.com>
2021-12-05 14:31:22 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+import express from 'express';
+import dotenv from "dotenv";
+
+const port = process.env.PORT || 3000;
+const databaseUrl = process.env.CLEARDB_DATABASE_URL || 'None';
+
+dotenv.config({
+    path: ".env",
+});
+
+const app = express();
+
+app.get('/', function (req, res) {
+  res.send(`Database is running on: ${databaseUrl}`);
+})
+
+app.listen(port, () => {
+  console.log(`API Rodando na porta: (${port})..`);
+  console.log(`Database is running on: ${databaseUrl}`);
+});
