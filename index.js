590288c8612d00afb1f97aeb4e6ca5e0f154ab08
Daniel Wust <danielwust99@gmail.com>
2022-01-09 09:28:54 -0300

    adicionado novo branch de testes

--- index.js
+++ index.js
@@ -14,6 +14,18 @@ app.get('/', function (req, res) {
   res.send(`Database is running!`);
 })
 
+app.get("/Command", function (req, res) {
+  res.send({
+    "cron_string": "1 * * * * *",
+    "destination": "#input",
+    "source": "#output",
+    "flags": "a",
+    "id": 1,
+    "params": "any",
+    "execute": true
+  });
+});
+
 app.listen(port, () => {
   console.log(`API Rodando na porta: (${port})..`);
   console.log(`Database is running on: ${databaseUrl}`);
